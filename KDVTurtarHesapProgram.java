9085e4eb8a3f0d42672a4fcee2801bcdb9d534cf
nisahekimoglu <100082081+nisahekimoglu@users.noreply.github.com>
2022-07-31 21:21:40 +0300

    Update KDVTurtarHesapProgram.java

--- KDVTurtarHesapProgram.java
+++ KDVTurtarHesapProgram.java
@@ -5,11 +5,13 @@ public class KDVTurtarHesapProgram {
 		// TODO Auto-generated method stub
 Scanner k = new Scanner(System.in);
 System.out.println("tutar giriniz");
-double tutar = k.nextDouble();
-double kdv =0.18;
-double kdvoran = tutar* kdv;
-double kdvlitutar = kdvoran+ tutar;
-System.out.println("kdvli tutar " + kdvlitutar);
+double ilktutar = k.nextDouble();
+
+double katsayi = ilktutar<1000 ? 0.18 : 0.8 ;
+double kdvtutar = katsayi*ilktutar;
+double sontutar = kdvtutar + ilktutar;
+
+System.out.println("kdvli tutar " + sontutar);
 
 
 	}
