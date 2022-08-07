959495297d6912d39e100888126edfb269b39a68
Nisa Hekimoğlu <100082081+nisahekimoglu@users.noreply.github.com>
2022-08-07 23:37:00 +0300

    Add files via upload

---
+++ KullaniciGirisiProgrami.java
@@ -0,0 +1,85 @@
+import java.util.Scanner;
+public class KullaniciGirisiProgrami {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String kullaniciAdi = "nisa";
+		String kullaniciSifre = "0987";	
+		
+    Scanner k = new Scanner(System.in);
+   
+    System.out.print("Kullanici adinizi giriniz: ");
+    String admin= k.nextLine();
+    
+    System.out.print("Sifrenizi giriniz: ");
+    String parola = k.nextLine();
+    
+    
+    
+         if (!kullaniciAdi.equals(admin) && !kullaniciSifre.equals(parola)) {
+        	 System.out.println("Kullanici adi ve parola yanlis tekrar deneyiniz "); 
+    	   
+         }
+         
+         else if (kullaniciAdi.equals(admin) && !kullaniciSifre.equals(parola)) {
+        	 System.out.println("Parola yanlis , sifirlamak ister misiniz ? "); 
+        	
+     	     System.out.println("evet/ hayir" );
+     	     
+     	    String kullCevap =k.nextLine();
+         	
+            
+        	
+        	// System.out.println(kullCevap);
+        	 
+            String cevap = "evet";
+         	 boolean sonuc = cevap == kullCevap;
+        	 
+        	 System.out.println("Yeni sifrenizi giriniz");
+        	 String yeniSifre = k.nextLine();
+      	   
+        	 
+        	     if (yeniSifre.equals(kullaniciSifre)) {
+        		 System.out.println("Eski parola ile yeni parola ayni olamaz, tekrar deneyiniz ");
+       		 
+        	      }
+        	 
+        	     else {
+        		 System.out.println("Yeni sifre olusturuldu"); 
+        	     }
+        	
+         }
+      	 
+      	 else {
+      		System.out.println("Parola dogru giris basarili ");  
+      		
+           }
+      	 
+      
+      	 
+      	 
+      	 
+      	 
+      	 
+      	 
+      
+         
+
+    
+
+
+
+
+
+
+/*
+ * Kullanici adi ve sifeyi kontrol eden programi yapiniz
+ * Eğer şifre yanlış ise kullanıcıya şifresini sıfırlayıp sıfırlamayacağını sorun, 
+ * eğer kullanıcı sıfırlamak isterse yeni girdiği şifrenin hatalı girdiği ve unuttuğu şifre ile 
+ * aynı olmaması gerektiğini kontrol edip , şifreler aynı ise ekrana
+ * "Şifre oluşturulamadı, lütfen başka şifre giriniz." sorun yoksa "Şifre oluşturuldu" yazan programı yazınız.
+ */
+	}
+
+}
