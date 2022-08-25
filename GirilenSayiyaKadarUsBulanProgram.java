7bca1e943e617950ce0104e6db8f204682c135c4
Nisa Hekimoğlu <100082081+nisahekimoglu@users.noreply.github.com>
2022-08-26 00:33:49 +0300

    Add files via upload

---
+++ GirilenSayiyaKadarUsBulanProgram.java
@@ -0,0 +1,60 @@
+import java.util.Scanner;
+public class GirilenSayiyaKadarUsBulanProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       Scanner k = new Scanner(System.in);
+   int i;
+   int j;
+  
+     	 System.out.println("sayi giriniz : ");
+     	 int sayi = k.nextInt();
+     	
+     	
+   
+     	for (i=1 ; i<sayi ;i*=4) {
+     		System.out.println(i); 	 
+     	 }
+     	 
+     	System.out.println("==========="); 	 
+     	
+     	for (j=1 ; j<sayi ;j*=5) {
+     		System.out.println(j); 	 
+     	 }
+     	
+      
+   
+       
+ 
+     		    
+     		 
+     	
+    /*
+      
+    do {
+    	 System.out.println("sayi giriniz : ");
+    	 sayi = k.nextInt();
+    	 
+   
+    	for (i=1 ; i<sayi ; i*=2) {
+    		System.out.println(i); 	 
+    	 }
+      }
+  
+       while (i<sayi);	
+    		    
+     
+   */		
+		
+		
+		
+		
+		
+		
+		
+	
+		
+		
+	}
+
+}
